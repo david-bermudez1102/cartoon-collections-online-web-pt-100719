aaf36bc0ca4e06998b5736a45f05ad6c8c1ead5b
David Bermudez <juan.bermudez1102@outlook.com>
2019-10-09 13:23:33 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -11,8 +11,11 @@ def summon_captain_planet(array)
   end
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
+def long_planeteer_calls(calls)
+  longer_than_four = calls.all? { |call|  
+    call.length > 4
+  }
+  longer_than_four
 end
 
 def find_the_cheese# code an argument here
